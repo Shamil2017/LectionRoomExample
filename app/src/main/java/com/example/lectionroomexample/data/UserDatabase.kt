571e31110c37449ff1125c7df6947de396ae8567
OtsokovShA <shamil24@mail.ru>
2023-11-21 17:42:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lectionroomexample/data/UserDatabase.kt
@@ -0,0 +1,8 @@
+package com.example.lectionroomexample.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [User::class], version = 1, exportSchema = false)
+abstract class UserDatabase: RoomDatabase() {
+}
